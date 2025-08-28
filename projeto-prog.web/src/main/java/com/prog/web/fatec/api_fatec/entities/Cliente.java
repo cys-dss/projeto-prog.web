a1dfe8c08988ce26919279ef2352f30553567ea9
Cayos Souza <cayossouza2005@gmail.com>
2025-08-27 21:56:14 -0300

    Banco lacração

--- projeto-prog.web/src/main/java/com/prog/web/fatec/api_fatec/entities/Cliente.java
+++ projeto-prog.web/src/main/java/com/prog/web/fatec/api_fatec/entities/Cliente.java
@@ -1,27 +1,52 @@
 package com.prog.web.fatec.api_fatec.entities;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="clientes")
 public class Cliente {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-   
+    
+    @Column(nullable = false, length = 60)
     private String nome;
- 
+
+    @Column(nullable = false, length = 60)
+    private String endereco;
+    
     public Cliente() {
-       
+        
     }
- 
+
+    public Cliente(Long cod, String nome, String endereco) {
+        this.id = cod;
+        this.nome = nome;
+        this.endereco = endereco;
+    }
+
     public Long getId() {
         return id;
     }
- 
+
     public void setId(Long id) {
         this.id = id;
     }
- 
+
     public String getNome() {
         return nome;
     }
- 
+
     public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
     }
 }
